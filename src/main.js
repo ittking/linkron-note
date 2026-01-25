84d7c4e26d758b1cb9b3de413297bd8253cda52b
码农无码 <3292786529@qq.com>
2026-01-25 19:28:44 +0800

    chore: 移除 Ant Design Vue 并设置全局高度样式
    - 移除 Ant Design Vue 及其相关导入与使用，简化依赖
    - 在样式文件中为 html、body 和 #app 元素添加全高度样式

--- src/main.js
+++ src/main.js
@@ -3,13 +3,8 @@ import App from "./App.vue";
 import router from "./router";
 
 import "./style.css";
-import Antd from "ant-design-vue";
-import "ant-design-vue/dist/reset.css";
 
 const app = createApp(App);
-
-// 使用 Ant Design Vue
-app.use(Antd);
 
 // 使用 Router
 app.use(router);
