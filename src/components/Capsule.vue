024b05ebacc0f508f18001339d98a6a634aa799c
码农无码 <3292786529@qq.com>
2026-01-29 16:38:09 +0800

    style(Capsule): 调整右侧间距并提高背景透明度
    将胶囊组件右侧间距从 0 调整为 1，并将背景透明度从 50% 提高至 80%，以改善视觉层次和可读性。

--- src/components/Capsule.vue
+++ src/components/Capsule.vue
@@ -71,9 +71,9 @@ onUnmounted(() => {
 </script>
 
 <template>
-  <div data-tauri-drag-region ref="capsuleElement" class="absolute top-0 right-0 z-[1000]">
+  <div data-tauri-drag-region ref="capsuleElement" class="absolute top-0 right-1 z-[1000]">
     <div data-tauri-drag-region through-listener="true"
-      class="flex items-center gap-2 p-1 bg-base-100 border border-base-300 bg-base-100/50 rounded-full shadow-lg backdrop-blur select-none">
+      class="flex items-center gap-2 p-1 bg-base-100 border border-base-300 bg-base-100/80 rounded-full shadow-lg backdrop-blur select-none">
       <div data-tauri-drag-region class="flex items-center gap-2 text-xs text-base-content pl-2">
         <span data-tauri-drag-region class="font-semibold text-primary">{{ currentDate }}</span>
         <div data-tauri-drag-region class="flex flex-col items-center text-base-content/70 scale-80">
